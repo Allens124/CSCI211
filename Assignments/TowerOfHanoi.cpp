d23e6d9cc6f800fbd0a4fb2b6adb6eb85f1d5153
Allens124 <124allens@gmail.com>
2024-11-11 13:38:54 -0500

    condition checking

--- Assignments/TowerOfHanoi.cpp
+++ Assignments/TowerOfHanoi.cpp
@@ -6,6 +6,11 @@ int main()
     int n;
     cout << "Enter the number of rings: ";
     cin >> n;
+    while (n < 1)
+    {
+        cout << "Invalid input! Try again: ";
+        cin >> n;
+    }
     vector <int> t[3];
     return 0;
 }
