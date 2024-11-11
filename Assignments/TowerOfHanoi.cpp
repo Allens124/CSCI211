f8b18cc5fb9ff5c95c6c39c1eaf12d283b156107
Allens124 <124allens@gmail.com>
2024-11-11 14:01:49 -0500

    cleaning up code

--- Assignments/TowerOfHanoi.cpp
+++ Assignments/TowerOfHanoi.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 int main()
 {
-    int n;
+    vector <int> t[3];
+    int n, to, from;
     cout << "Enter the number of rings: ";
     cin >> n;
     while (n < 1)
@@ -11,7 +12,6 @@ int main()
         cout << "Invalid input! Try again: ";
         cin >> n;
     }
-    vector <int> t[3];
     for (int i = n+1; i > 0; i--)
     {
         t[0].push_back(i);
