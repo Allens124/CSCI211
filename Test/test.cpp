67e701a54ecde453e1dfd4bfe4ce641a33c20205
Allens124 <124allens@gmail.com>
2024-11-11 01:52:49 -0500

    Adding colors

--- Test/test.cpp
+++ Test/test.cpp
@@ -5,14 +5,14 @@ int main()
     string s = "Hello World!";
     for (int i = 0; i < s.length(); i++)
     {
-        for (int j = 0; j < 20000; j++)
+        for (int j = 0; j < 10000; j++)
         {
             for (char k = '!'; k < '~'; k++)
             {
                 cout << k << char(8);
             }
         }
-        cout << s[i];
+        cout << "\033[31m" << s[i];
     }
     cout << endl;
     return 0;
