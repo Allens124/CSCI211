ec081c5f5eacd4b11706a47afb7138bae4f1e3ae
Allens124 <124allens@gmail.com>
2024-11-11 02:09:49 -0500

    Fixing colors

--- Test/test.cpp
+++ Test/test.cpp
@@ -5,20 +5,20 @@ using namespace std;
 
 string colors[8] = 
 {
-    "\033[30m", // black
     "\033[31m", // red
-    "\033[32m", // green
     "\033[33m", // yellow
+    "\033[32m", // green
+    "\033[36m", // cyan
     "\033[34m", // blue
     "\033[35m", // purple
-    "\033[36m", // cyan
+    "\033[30m", // black
     "\033[37m" // white
 };
 
 void printMessage(string str)
 {
     srand(time(0));
-    int r = rand()%7;
+    int r = rand()%8;
     for (int i = 0; i < str.length(); i++)
     {
         for (int j = 0; j < 10000; j++)
@@ -28,7 +28,7 @@ void printMessage(string str)
                 cout << k << char(8);
             }
         }
-        cout << colors[1] << str[i];
+        cout << colors[(r+i)%8] << str[i];
     }
     cout << endl;
 }
