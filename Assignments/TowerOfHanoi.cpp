92a861e0b4664713bd732926a5c9d27091659750
Allens124 <124allens@gmail.com>
2024-11-12 03:02:52 -0500

    fixing output statements

--- Assignments/TowerOfHanoi.cpp
+++ Assignments/TowerOfHanoi.cpp
@@ -5,9 +5,9 @@ int main()
 {
     vector <int> tower[3];
     int from = 0, to = 1, candidate = 1, move = 0, ring;
-    cout << "Enter the number of rings: ";
+    cout << "Enter an odd number of rings: ";
     cin >> ring;
-    while (ring < 1)
+    while (ring%2 == 0 || ring < 0)
     {
         cout << "Invalid input! Try again: ";
         cin >> ring;
@@ -18,9 +18,9 @@ int main()
     }
     tower[1].push_back(ring+1);
     tower[2].push_back(ring+1);
-    while (tower[1].size() < ring+1 && tower[2].size() < ring+1)
+    while (tower[1].size() < ring+1)
     {
-        cout << "Move number " << ++move << ": Transfer ring " << candidate << " from tower " << char(from+'A') << " to tower " << char(to+'A') << endl;
+        cout << "Move " << ++move << ": Transfer ring " << candidate << " from Tower " << char(from+'A') << " to Tower " << char(to+'A') << endl;
         tower[to].push_back(candidate);
         tower[from].pop_back();
         if (tower[(to+1)%3].back() < tower[(to+2)%3].back())
