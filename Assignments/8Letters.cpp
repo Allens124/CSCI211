aa0c58459d3a400d6a8408e4ef2c6e42d51a1ef4
Allens124 <124allens@gmail.com>
2024-11-15 02:23:58 -0500

    initializing adjacency array

--- Assignments/8Letters.cpp
+++ Assignments/8Letters.cpp
@@ -3,8 +3,15 @@ using namespace std;
 
 char adj =
 {
-    
-}
+    {'z'},
+    {'a', 'z'},
+    {'a', 'z'},
+    {'a', 'b', 'c', 'z'},
+    {'a', 'b', 'd', 'z'},
+    {'b', 'e', 'z'},
+    {'c', 'd', 'e', 'z'},
+    {'d', 'e', 'f', 'g', 'z'}
+};
 
 int main()
 {
