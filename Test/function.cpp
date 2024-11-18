5461b8a415830f05ec8f8ec52949367edd24f292
Allens124 <124allens@gmail.com>
2024-11-18 00:45:31 -0500

    working on bubbleSort function

--- Test/function.cpp
+++ Test/function.cpp
@@ -4,6 +4,23 @@ using namespace std;
 typedef bool (*func)(bool);
 
 void bubbleSort(int arr, int size)
+{
+    bool swap = false;
+    for (int i = 0; i < size; i++)
+    {
+        swap = false;
+        for (int j = 0; j < size; j++)
+        {
+            if (arr[i] < arr[j])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                swap = true;
+            }
+        }
+    }
+}
 
 int main()
 {
