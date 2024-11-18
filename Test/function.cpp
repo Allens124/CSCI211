9667d81e5ac057dc36ef7669974e7e2ff8466cf2
Allens124 <124allens@gmail.com>
2024-11-18 00:49:40 -0500

    fixing bubbleSort function

--- Test/function.cpp
+++ Test/function.cpp
@@ -5,19 +5,27 @@ using namespace std;
 
 typedef bool (*func)(bool);
 
+void fillRandom(int arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int )
+    }
+}
+
 void bubbleSort(int arr, int size)
 {
     bool swap = true;
     while (swap)
     {
         swap = false;
-        for (int j = 0; j < size; j++)
+        for (int i = 0; i < size-1; i++)
         {
-            if (arr[i] < arr[j])
+            if (arr[i] < arr[i+1])
             {
                 int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
                 swap = true;
             }
         }
