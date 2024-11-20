845d77a8512894ab8267a45b57b64c7ed3de24f7
Allens124 <124allens@gmail.com>
2024-11-20 10:56:47 -0500

    editing functions

--- Test/test.cpp
+++ Test/test.cpp
@@ -6,7 +6,6 @@ void clear()
     for (int i = 0; i < 9; i++)
     {
         cout << char(8);
-        for (int j = 0; j < 10000; j++);
     }
 }
 
@@ -15,7 +14,6 @@ void frame1()
     for (int i = 0; i < 9; i++)
     {
         cout << "*";
-        for (int j = 0; j < 10000; j++);
     }
 }
 
