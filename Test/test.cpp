75e71ed2a3df44faca4ed9ab59f73b3402523f18
Allens124 <124allens@gmail.com>
2024-11-20 10:52:17 -0500

    filling frame1 function

--- Test/test.cpp
+++ Test/test.cpp
@@ -11,12 +11,16 @@ void clear()
 
 void frame1()
 {
-    
+    for (int i = 0; i < 9; i++)
+    {
+        cout << "*";
+        for (int j = 0; j < 10000; j++);
+    }
 }
 
 void frame2()
 {
-
+    //code;
 }
 
 int main()
