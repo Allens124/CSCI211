07f8d3658040d29812e8de1984b3118709cddf7d
Allens124 <124allens@gmail.com>
2024-11-20 10:59:02 -0500

    removing functions

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,31 +1,8 @@
 #include <iostream>
 using namespace std;
 
-void clear()
-{
-    for (int i = 0; i < 9; i++)
-    {
-        cout << char(8);
-    }
-}
-
-void frame1()
-{
-    for (int i = 0; i < 9; i++)
-    {
-        cout << "*" << char(8);
-    }
-}
-
-void frame2()
-{
-    //code;
-}
-
 int main()
 {
-    frame1();
-    clear();
-    cout << endl;
+    cout << "Hi" << char(8) << endl;
     return 0;
 }
