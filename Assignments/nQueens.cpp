870a63b5d4ece1601c5738d8fdba03e629a3f3cc
Allens124 <124allens@gmail.com>
2024-11-20 19:10:36 -0500

    implementing exception handling with a while loop

--- Assignments/nQueens.cpp
+++ Assignments/nQueens.cpp
@@ -8,5 +8,10 @@ int main()
     int n;
     cout << "How many queens? ";
     cin >> n;
+    while (n < 1)
+    {
+        cout << "Invalid input! Try again: ";
+        cin >> n;
+    }
     return 0;
 }
