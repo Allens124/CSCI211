5c05e7c221f5e6664ccd112866139d10f74d84db
Allens124 <124allens@gmail.com>
2024-11-22 10:45:30 -0500

    declaring and calling print function

--- Test/3DQueens.cpp
+++ Test/3DQueens.cpp
@@ -3,7 +3,18 @@ using namespace std;
 
 int solCount = 0;
 
+void print(int q[8][8])
+{
+
+}
+
 void eightQueens(int q[8][8], int l, int c)
+{
+    if (l == 8)
+    {
+        print(q);
+    }
+}
 
 int main()
 {
