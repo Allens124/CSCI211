de2e4abd8f8e2ec5d594e9860e6a3bbca67fee8f
Allens124 <124allens@gmail.com>
2024-11-23 07:20:32 -0500

    taking in user input and handling exceptions

--- Test/LatinSquare.cpp
+++ Test/LatinSquare.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-    //code;
+    int n;
+    cout << "How many rows and columns? ";
+    cin >> n;
+    if (n < 1)
+    {
+        cout << "Invalid input! Try again: ";
+        cin >> n;
+    }
     return 0;
 }
