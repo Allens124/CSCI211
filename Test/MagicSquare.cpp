8ced6186399b082045c63fbe3fbb684897f21761
Allens124 <124allens@gmail.com>
2024-11-24 15:50:21 -0500

    created magicSum function

--- Test/MagicSquare.cpp
+++ Test/MagicSquare.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
 
-
+int magicSum(int cap)
+{
+    int sum = 0;
+    for (int i = 1; i < cap+1; i++)
+    {
+        sum += i;
+    }
+    return sum/cap;
+}
 
 int main()
 {
