9d3070618ffb9e4808d772aedd1c941488900b0f
Allens124 <124allens@gmail.com>
2024-11-25 13:47:18 -0500

    handling exceptions for when n is invalid

--- Labs/DynamicMemory.cpp
+++ Labs/DynamicMemory.cpp
@@ -5,5 +5,10 @@ int main()
     int n;
     cout << "Enter the size of a 2D array: ";
     cin >> n;
+    while (n < 1)
+    {
+        cout << "Invalid input! Try again: ";
+        cin >> n;
+    }
     return 0;
 }
