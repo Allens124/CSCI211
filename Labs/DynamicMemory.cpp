f3471523f343cde9ee5c95918ef38fea551c9fe4
Allens124 <124allens@gmail.com>
2024-11-25 13:52:34 -0500

    filling and printing 2D array

--- Labs/DynamicMemory.cpp
+++ Labs/DynamicMemory.cpp
@@ -10,10 +10,20 @@ int main()
         cout << "Invalid input! Try again: ";
         cin >> n;
     }
+    int fill = n*n;
     int** arr = new int*[n];
     for (int i = 0; i < n; i++)
     {
         arr[i] = new int[n];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i][j] = fill--;
+            cout << arr[i][j];
+        }
+        cout << endl;
     }
     for (int i = 0; i < n; i++)
     {
