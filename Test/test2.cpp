ba1fbdd67774d83628c22cf15fe236d4341f3ec2
Allens124 <124allens@gmail.com>
2024-11-27 02:00:21 -0500

    added nested for loop to compare results

--- Test/test2.cpp
+++ Test/test2.cpp
@@ -3,6 +3,14 @@ using namespace std;
 int main()
 {
     int arr[3][3] = {0};
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << *(*(arr+i)+j) << " ";
+        }
+        cout << endl;
+    }
     for (int i = 0; i < 3*3; i++)
     {
         cout << *(*(arr+i)) << " ";
