b431b57dde93913b007ce74e197da1b98195843b
Allens124 <124allens@gmail.com>
2024-11-27 01:58:29 -0500

    fixed for loop

--- Test/test2.cpp
+++ Test/test2.cpp
@@ -5,7 +5,7 @@ int main()
     int arr[3][3] = {0};
     for (int i = 0; i < 3*3; i++)
     {
-        cout << *(*(arr+i)) " ";
+        cout << *(*(arr+i)) << " ";
     }
     cout << endl;
     return 0;
