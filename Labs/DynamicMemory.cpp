2e79ef26ffd56406284bee310eef26bfe16ce858
Allens124 <124allens@gmail.com>
2024-11-27 02:18:46 -0500

    editing comments

--- Labs/DynamicMemory.cpp
+++ Labs/DynamicMemory.cpp
@@ -13,7 +13,7 @@ int main()
         cout << "Invalid input! Try again: ";
         cin >> n;
     }
-    // Declare and dynamically allocate memory to a 2D array
+    // Declare and dynamically allocate memory to the 2D array
     int** arr = new int*[n];
     // Using a for loop, dynamically allocate memory to each row of the 2D array
     for (int i = 0; i < n; i++)
