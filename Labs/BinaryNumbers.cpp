fb79083af4cfd667edfd7b6d077848041ab57a73
Allens124 <124allens@gmail.com>
2024-11-28 00:05:16 -0500

    renamed recursive function

--- Labs/BinaryNumbers.cpp
+++ Labs/BinaryNumbers.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int toBinary(int n)
+int decimalToBinary(int n)
 {
     if (n < 2)
     {
         return n%2;
     }
-    return 10*toBinary(n/2)+n%2;
+    return 10*decimalToBinary(n/2)+n%2;
 }
 
 int main()
 {
     for (int i = 0; i <= 16; i++)
     {
-        cout << toBinary(i) << endl;
+        cout << decimalToBinary(i) << endl;
     }
     return 0;
 }
