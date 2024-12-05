1839e25bc4d585d1650fc7d4f3aaa17c08202b1b
Allens124 <124allens@gmail.com>
2024-12-04 23:36:21 -0500

    created a contains function

--- Test/Recaman.cpp
+++ Test/Recaman.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+bool contains(vector <int> v, int n)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        if (v[i] == n)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main()
 {
     vector <int> r(1);
