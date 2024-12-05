c5f335f26228852038fdd56d165e0282132ebea6
Allens124 <124allens@gmail.com>
2024-12-04 23:25:01 -0500

    renamed function

--- Test/Jumbled.cpp
+++ Test/Jumbled.cpp
@@ -2,19 +2,19 @@
 #include <cmath>
 using namespace std;
 
-bool isUnjumbled(int n)
+bool isJumbled(int n)
 {
     if (n < 10)
     {
         return false;
     }
-    return isUnjumbled(n/10) || abs((n/10)%10-n%10) < 2;
+    return isJumbled(n/10) || abs((n/10)%10-n%10) > 1;
 }
 
 int main()
 {
-    cout << isUnjumbled(6765) << endl;
-    cout << isUnjumbled(1223) << endl;
-    cout << isUnjumbled(1235) << endl;
+    cout << isJumbled(6765) << endl;
+    cout << isJumbled(1223) << endl;
+    cout << isJumbled(1235) << endl;
     return 0;
 }
