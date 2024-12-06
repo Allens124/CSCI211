2ba87ba03d397a50a2cfaf0d61b424d4be17b25b
Allens124 <124allens@gmail.com>
2024-12-06 05:55:17 -0500

    filled in addedPolynomials function

--- Test/Polynomial.cpp
+++ Test/Polynomial.cpp
@@ -4,7 +4,21 @@ using namespace std;
 
 string addPolynomials(vector <int> p, vector <int> q)
 {
-    
+    int resultInt = 0;
+    string resultString = "";
+    for (int i = 0; i < p.size() || q.size(); i++)
+    {
+        resultInt = p[i]+q[i];
+        while (resultInt > 0)
+        {
+            resultString += resultInt%10+'0';
+            resultInt /= 10;
+        }
+        resultString += "x^";
+        resultString += char(i+'0');
+        resultString += " ";
+    }
+    return resultString;
 }
 
 int main()
