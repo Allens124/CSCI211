d92a3fdd575e0a809d8c757845802f65963d4d94
Allens124 <124allens@gmail.com>
2024-12-07 20:18:56 -0500

    created fibonacci function using memoization

--- Test/Fibonacci.cpp
+++ Test/Fibonacci.cpp
@@ -2,6 +2,25 @@
 #include <vector>
 using namespace std;
 
+int fibonacci(int n)
+{
+    static int term[50];
+    if (n < 2)
+    {
+        
+        return n;
+    }
+    if (term[n] != 0)
+    {
+        return term[n];
+    }
+    else
+    {
+        term[n] = fibonacci(n-1)+fibonacci(n-2);
+        return term[n];
+    }
+}
+
 int main()
 {
     vector <int> f;
