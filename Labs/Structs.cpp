01c53a8522725f70334f37858b6e4a2cf1069bc8
Allens124 <124allens@gmail.com>
2024-12-11 14:13:13 -0500

    removed index variable

--- Labs/Structs.cpp
+++ Labs/Structs.cpp
@@ -5,32 +5,27 @@ using namespace std;
 struct Student
 {
     string name;
-    double midGrade;
-    double finGrade;
+    double mid;
+    double fin;
 };
 
 int main()
 {
-    int i = 0;
     string sName;
-    double sGrade;
+    double midGrade, finGrade;
     vector <Student> students;
     while (true)
     {
         cout << "Enter the first name of a student: ";
         cin >> sName;
-        students[i].name = sName;
         if (sName == "")
         {
             break;
         }
-        cout << "Enter the midterm grade of the student: ";
-        cin >> sGrade;
-        students[i].midGrade = sGrade;
+        cout << "Enter the midterm grade of the studnet: ";
+        cin >> midGrade;
         cout << "Enter the final grade of the student: ";
-        cin >> sGrade;
-        students[i].finGrade = sGrade;
-        i++;
+        cin >> finGrade;
     }
     return 0;
 }
