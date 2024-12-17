f41ca0f12d7a774da35236030db56f1394d8ab36
Allens124 <124allens@gmail.com>
2024-12-16 21:15:06 -0500

    created a program for practicing dynamic memory allocation

--- Labs/prac1.cpp
+++ Labs/prac1.cpp
@@ -1,7 +1,26 @@
+/*
+DYNAMIC MEMORY ALLOCATION PRACTICE
+*/
+
 #include <iostream>
 using namespace std;
 int main()
 {
-    // code;
+    // Dynamically allocate memory to an integer array of size 5 called arr1
+
+    // Fill arr1 with the numbers 1, 3, 5, 7, and 9
+
+    // Print each element of arr1, separated by a space. End the line after printing
+
+    // Dynamically allocate memory to an integer array of size 5 called arr2
+
+    // Fill arr2 with the numbers 0, 1, 4, 9, and 16
+
+    // Print each element of arr2, separated by a space. End the line after printing
+
+    // Print the sum of the elements in arr1 and the corresponding elements in arr2, separated by a space. End the line after printing
+
+    // Deallocate memory from arr1 and arr2
+
     return 0;
 }
