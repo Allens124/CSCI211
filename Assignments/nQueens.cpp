347090f29ff12a3174e51b00e98ece98a25596d2
Allens124 <124allens@gmail.com>
2024-12-16 19:56:49 -0500

    filled in the for loop with the row test

--- Assignments/nQueens.cpp
+++ Assignments/nQueens.cpp
@@ -3,6 +3,13 @@ using namespace std;
 
 bool ok(int* q, int c, int cap)
 {
+    for (int i = 0; i < c; i++)
+    {
+        if (q[i] == q[c])
+        {
+            return false;
+        }
+    }
     return true;
 }
 
