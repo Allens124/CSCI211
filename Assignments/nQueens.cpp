41e4fb5d0c41cf5411bb19a8840cff1d50ac504f
Allens124 <124allens@gmail.com>
2024-12-16 19:58:14 -0500

    filled in the for loop with the diagonal tests

--- Assignments/nQueens.cpp
+++ Assignments/nQueens.cpp
@@ -6,7 +6,7 @@ bool ok(int* q, int c)
 {
     for (int i = 0; i < c; i++)
     {
-        if (q[i] == q[c])
+        if (q[i] == q[c] || c-i == abs(q[c]-q[i]))
         {
             return false;
         }
