fcb78bcbf209948020d14a682b1f51e5e80daa94
Allens124 <124allens@gmail.com>
2024-12-16 19:54:17 -0500

    fixed the conditions of the for loop

--- Assignments/nQueens.cpp
+++ Assignments/nQueens.cpp
@@ -17,9 +17,9 @@ int main()
         cout << "Invalid input! Try again: ";
         cin >> n;
     }
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        
+
     }
     return 0;
 }
