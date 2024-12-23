d5bcf74f48b665eed5894dd6d5280181d3073bc9
Allens124 <124allens@gmail.com>
2024-12-22 19:52:20 -0500

    added test cases to the main function

--- Test/test3.cpp
+++ Test/test3.cpp
@@ -5,11 +5,14 @@ template <typename T>
 
 T add(T a, T b)
 {
-    
+    return a+b;
 }
 
 int main()
 {
-    //code;
+    cout << add(2, 3) << endl;
+    cout << add(3.14, 3.14) << endl;
+    cout << add("He", "llo") << endl;
+    cout << add('H', 'i') << endl;
     return 0;
 }
