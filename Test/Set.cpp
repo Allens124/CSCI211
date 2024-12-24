6f89c0e4dc490717df153791911f9692378e5bfd
Allens124 <124allens@gmail.com>
2024-12-23 20:51:13 -0500

    created a cap instance variable as well as a private sort() method

--- Test/Set.cpp
+++ Test/Set.cpp
@@ -6,7 +6,26 @@ template <typename T>
 class Set
 {
     private:
+        int cap;
         T arr[100];
+        void sort()
+        {
+            bool swap;
+            while (swap)
+            {
+                swap = false;
+                for (int i = 0; i < cap; i++)
+                {
+                    if (arr[i] > arr[i+1])
+                    {
+                        T temp = arr[i];
+                        arr[i] = arr[i+1];
+                        arr[i+1] = temp;
+                        swap = true;
+                    }
+                }
+            }
+        }
 };
 
 int main()
