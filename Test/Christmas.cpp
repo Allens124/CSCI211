dcf31188376cf06240774e38845e619a3dd24886
Allens124 <124allens@gmail.com>
2024-12-26 21:05:27 -0500

    handling exceptions for when the value of n is invalid

--- Test/Christmas.cpp
+++ Test/Christmas.cpp
@@ -8,5 +8,10 @@ int main()
     int n;
     cout << "Enter the size of the tree: ";
     cin >> n;
+    while (n < 1)
+    {
+        cout << "Invalid input! Try again: ";
+        cin >> n;
+    }
     return 0;
 }
