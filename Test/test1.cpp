dfa0e637538c6ab17240f427fa923de1e7cbf552
Allens124 <124allens@gmail.com>
2024-12-28 19:30:17 -0500

    handling exceptions for when size is invalid

--- Test/test1.cpp
+++ Test/test1.cpp
@@ -8,5 +8,10 @@ int main()
     int size;
     cout << "Enter the sample size: ";
     cin >> size;
+    while (size < 1)
+    {
+        cout << "Invalid input! Try again: ";
+        cin >> size;
+    }
     return 0;
 }
