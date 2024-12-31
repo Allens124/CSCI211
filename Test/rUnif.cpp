921068f76a8810887e87e31dbf79ace693a93820
Allens124 <124allens@gmail.com>
2024-12-30 22:11:49 -0500

    made the sample array static in the rUnif() function

--- Test/rUnif.cpp
+++ Test/rUnif.cpp
@@ -16,7 +16,7 @@ double* rUnif(int n, int min = 0, int max = 1)
         min = temp;
     }
     srand(time(0));
-    double* sample = new double[n];
+    static double sample[n];
     for (int i = 0; i < n; i++)
     {
         sample[i] = 0.001*(rand()%(1000*(max-min)+1))+min;
@@ -31,6 +31,5 @@ int main()
     {
         cout << i+1 << ": " << unifSample[i] << endl;
     }
-    delete[] unifSample;
     return 0;
 }
