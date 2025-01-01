28902bb8e831b4277e370c391fbbb71c0cf838f6
Allens124 <124allens@gmail.com>
2024-12-31 23:57:53 -0500

    using a for loop, pushed back every element of each vector into the result vector

--- Test/test2.cpp
+++ Test/test2.cpp
@@ -5,6 +5,14 @@ using namespace std;
 vector <int> operator+(vector <int> a, vector <int> b)
 {
     vector <int> result;
+    for (int i = 0; i < a.size(); i++)
+    {
+        result.push_back(a[i]);
+    }
+    for (int i = 0; i < b.size(); i++)
+    {
+        result.push_back(b[i]);
+    }
 }
 
 int main()
