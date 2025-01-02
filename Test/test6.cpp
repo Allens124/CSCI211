9437bd6d07565f30d493fb5625d0a15d6eced19d
Allens124 <124allens@gmail.com>
2025-01-02 10:02:12 -0500

    created a toString method that converts a digit into its binary representation

--- Test/test6.cpp
+++ Test/test6.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 using namespace std;
+
+string toString(int x)
+{
+    string result = " ";
+    if (x < 0)
+    {
+        return result;
+    }
+    else if (x < 10)
+    {
+        result[0] = '0'+x;
+        return result
+    }
+    result[0] = 'A'+x;
+    return result;
+}
+
 int main()
 {
     //code;
