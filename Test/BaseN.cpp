1c058bb653bdd408e77f11501817e2fba8feb0da
Allens124 <124allens@gmail.com>
2025-01-02 10:09:42 -0500

    added toString() function from test6.cpp

--- Test/BaseN.cpp
+++ Test/BaseN.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
 
-
+string toString(int x)
+{
+    string result = " ";
+    if (x > -1 && x < 10)
+    {
+        result[0] = '0'+x;
+    }
+    else if (x > 9 && x < 36)
+    {
+        result[0] = 'A'+x-10;
+    }
+    return result;
+}
 
 int main()
 {
