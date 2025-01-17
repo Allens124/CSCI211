3472ee9df46ca0279bec3e3df442113df780a8be
Allens124 <124allens@gmail.com>
2025-01-17 16:45:05 -0500

    checking output

--- Test/test8.cpp
+++ Test/test8.cpp
@@ -2,15 +2,11 @@
 using namespace std;
 int main()
 {
-    const int x = 5;
-    const int* p = &x;
-    int arr[3] = {1, *p, 3};
-    arr[0] = 10;
-    arr[1] = 4;
-    arr[2] = 7;
-    for (int i = 0; i < 3; i++)
+    const int x = 10;
+    const int* arr[1] = {&x};
+    for (int i = 0; i < 1; i++)
     {
-        cout << arr[i] << " ";
+        cout << *arr[i] << " ";
     }
     cout << endl;
     return 0;
