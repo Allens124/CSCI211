56d378ddfd8d56e6505a9ee4a4bf9c941635014a
Allens124 <124allens@gmail.com>
2025-01-20 19:26:41 -0500

    created temporary variables that store the value of the rightmost digit of a and b

--- Test/addStrings.cpp
+++ Test/addStrings.cpp
@@ -4,8 +4,13 @@ using namespace std;
 
 string addStrings(string a, string b)
 {
-    int aLength = a.size();
-    int bLength = b.size();
+    int aLength = a.size()-1;
+    int bLength = b.size()-1;
+    while (aLength > 0 || bLength > 0)
+    {
+        int aTemp = aLength < 0 ? a[aLength]-'0' : 0;
+        int bTemp = bLength < 0 ? b[bLength]-'0' : 0;
+    }
 }
 
 int main()
