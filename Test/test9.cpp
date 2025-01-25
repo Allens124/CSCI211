7252f97c3d2e341f74c7e188eeebbdfc3b43cb17
Allens124 <124allens@gmail.com>
2025-01-24 21:46:06 -0500

    using a nested for loop to print the graph of f(x) = x^2

--- Test/test9.cpp
+++ Test/test9.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 int main()
 {
-    //code;
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j <= i*i; j++)
+        {
+            if (j == i*i)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
     return 0;
 }
