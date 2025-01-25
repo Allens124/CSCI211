01f1d2024ade05c20bf164d8b75e81335d8cf87f
Allens124 <124allens@gmail.com>
2025-01-25 07:36:26 -0500

    changing the function to f(x) = log(x)

--- Test/test9.cpp
+++ Test/test9.cpp
@@ -6,9 +6,9 @@ int main()
     for (int i = -5; i <= 5; i++)
     {
         cout << i << "\t";
-        for (int j = -5; j <= (int)exp(i); j++)
+        for (int j = -5; j <= (int) log(i); j++)
         {
-            if (j == (int)exp(i))
+            if (j == (int) log(i))
             {
                 cout << "*";
             }
