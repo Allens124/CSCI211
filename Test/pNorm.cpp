512b39228a570a20c6a9ecbdc4f0a6e73c5cfb81
Allens124 <124allens@gmail.com>
2025-01-26 19:12:25 -0500

    checking output

--- Test/pNorm.cpp
+++ Test/pNorm.cpp
@@ -7,7 +7,7 @@ double pNorm(double lower, double upper, double mu, double sigma)
     double area = 0;
     double z1 = (lower-mu)/sigma;
     double z2 = (upper-mu)/sigma;
-    for (int i = z1; i <= z2; i += 0.001)
+    for (int i = z1; i <= z2; i += 0.1)
     {
         area += 1/sqrt(2*3.14159)*exp(-1*i*i);
     }
@@ -17,6 +17,6 @@ double pNorm(double lower, double upper, double mu, double sigma)
 int main()
 {
     cout << pNorm(-10, 0, 0, 1) << endl;
-    cout << pNorm(-10, 10, 0, 1) << endl;
+    //cout << pNorm(-10, 10, 0, 1) << endl;
     return 0;
 }
