98c6513b6fc0c11969ab6f17f8ea0ac1cebf556c
Allens124 <124allens@gmail.com>
2025-01-26 19:09:02 -0500

    computing the area under the curve of f(x) = 1/sqrt(2*pi) * exp(-x^2)

--- Test/pNorm.cpp
+++ Test/pNorm.cpp
@@ -2,10 +2,15 @@
 #include <cmath>
 using namespace std;
 
-double pNorm(double p, double mu, double sigma)
+double pNorm(double lower, double upper, double mu, double sigma)
 {
     double area = 0;
-    double z = (p-mu)/sigma;
+    double z1 = (lower-mu)/sigma;
+    double z2 = (upper-mu)/sigma;
+    for (int i = z1; i <= z2; i += 0.001)
+    {
+        area += 1/sqrt(2*3.14159)*exp(-1*i*i);
+    }
     return area;
 }
 
